5d3d6da67dfb12d9f02140548b6706d51adb1b51
何健伟 <jerry@hejianweideMBP.lan>
2017-11-23 19:57:13 +0800

    百度翻译：中英日

--- mac/Alfred.alfredpreferences/workflows/user.workflow.1E6DBA0A-5485-4BF3-AB7B-5420BD052E47/jTool/network.py
+++ mac/Alfred.alfredpreferences/workflows/user.workflow.1E6DBA0A-5485-4BF3-AB7B-5420BD052E47/jTool/network.py
@@ -12,6 +12,7 @@
 
 import urllib, urllib2, json, re
 
+# candidates
 def baiduCandidate(text):
     url = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su"
     params = {
@@ -25,7 +26,9 @@ def baiduCandidate(text):
     data = json.loads(data)
     candidates = data['s']
     for (i, e) in enumerate(candidates):
-        candidates[i] = e.encode('utf-8')
+        candidates[i] = e
+    if len(candidates) == 0:
+        candidates.append(text)
     return candidates
 
 def bingCandidate(text):
@@ -38,8 +41,12 @@ def bingCandidate(text):
     p = re.compile('(.*)<strong>(.*)</strong>', re.IGNORECASE)
     res = []
     for e in lis:
-        groups = p.match(e).groups()
-        res.append(groups[0] + groups[1])
+        matched = p.match(e)
+        if matched:
+            groups = matched.groups()
+            res.append(groups[0] + groups[1])
+    if len(res) == 0:
+        res.append(text)
     return res
 
 def googleCandidate(text):
@@ -63,10 +70,107 @@ def taobaoCandidate(text):
     res = data['result']
     candidates = []
     for e in res:
-        candidates.append(e[0].encode('utf-8'))
-    # for (i, e) in enumerate(candidates):
-    #     candidates[i] = e.encode('utf-8')
+        candidates.append(e[0])
+    if len(candidates) == 0:
+        candidates.append(text)
     return candidates
+
+
+# translate
+def baiduTranslateChinese(text):
+    return baiduTranslate(text,'zh')
+
+def baiduTranslateEnglish(text):
+    return baiduTranslate(text,'en', baiduTranslateEnglishSymbolHandler)
+
+def baiduTranslateEnglishSymbolHandler(data, inspect_res, correct_res):
+    ret = {
+        'res': '',
+        # correct text for input text
+        'correct': '',
+        # parts : [{means:'', part:''}], 'means' as translate results, 'part' as specific classification of words
+        'parts': [],
+        'audio': {}
+    }
+    ret['res'] = data['trans_result']['data'][0]['dst']
+    if correct_res['origtxt'] != correct_res['correctxt']:
+        ret['correct'] = correct_res['correctxt']
+
+    if data['dict_result'] and data['dict_result'].has_key('simple_means'):
+        symbols = data['dict_result']['simple_means']['symbols'][0]
+        parts = symbols['parts']
+        for (i, e) in enumerate(parts):
+            parts[i]['part'] = e['part_name'] + ('.' if len(e['part_name']) > 0 else '')
+            parts_means = parts[i]['means']
+            means = []
+            for (i2, e2) in enumerate(parts_means):
+                means.append(e2['text'])
+            parts[i]['means'] = means 
+        ret['parts'] = parts
+    return ret
+
+def baiduTranslateJapanese(text):
+    return baiduTranslate(text,'jp')
+
+def baiduTranslate(text, target_lan = 'zh', symbol_handler = False):
+    inspect_url = "http://fanyi.baidu.com/langdetect"
+    params = {
+        'query': text
+    }
+    inspect_res = syncAjax(inspect_url, params)
+    inspect_res = json.loads(inspect_res)
+
+    correct_url = "http://correctxt.baidu.com/correctxt?"
+    params = {
+        'callback':'cb',
+        'text':text, 
+        'ie': 'utf-8',
+        'version':0,
+        'from': 'FanyiWeb'
+    }
+    correct_res = syncAjax(correct_url, params)
+    correct_res = re.compile('^/\*\*/cb\((.*)\)$').match(correct_res).group(1)
+    correct_res = json.loads(correct_res)
+
+
+    translate_url = "http://fanyi.baidu.com/v2transapi"
+    params = {
+        'from': inspect_res['lan'],
+        'to': target_lan,
+        'query': text,
+        'transtype': 'translang',
+        'simple_means_flag': 3
+    }
+    data = syncAjax(translate_url, params)
+    data = json.loads(data)
+    ret = {
+        'res': '',
+        # correct text for input text
+        'correct': '',
+        # parts : [{means:'', part:''}], 'means' as translate results, 'part' as specific classification of words
+        'parts': [],
+        'audio': {}
+    }
+
+    if callable(symbol_handler):
+        ret = symbol_handler(data, inspect_res, correct_res)
+    else :
+        ret['res'] = data['trans_result']['data'][0]['dst']
+        if correct_res['origtxt'] != correct_res['correctxt']:
+            ret['correct'] = correct_res['correctxt']
+        
+        if data['dict_result'] and data['dict_result'].has_key('simple_means'):
+            symbols = data['dict_result']['simple_means']['symbols']
+            ret['parts'] = symbols[0]['parts']
+            for (i, e) in enumerate(ret['parts']):
+                means = ret['parts'][i]['means']
+                for (i2, e2) in enumerate(means):
+                    means[i2] = e2
+                ret['parts'][i]['part'] = e['part']
+    return ret
+
+
+
 
 
 def syncAjax(url, data = '', method = 'GET'):
@@ -82,6 +186,5 @@ def syncAjax(url, data = '', method = 'GET'):
         req = urllib2.Request(url)
     else:
         req = urllib2.Request(url, data)
-
     res = urllib2.urlopen(req).read()
     return res
--- mac/Alfred.alfredpreferences/workflows/user.workflow.1E6DBA0A-5485-4BF3-AB7B-5420BD052E47/jTool/output.py
+++ mac/Alfred.alfredpreferences/workflows/user.workflow.1E6DBA0A-5485-4BF3-AB7B-5420BD052E47/jTool/output.py
@@ -1,6 +1,44 @@
 import time, json, re
 import native
 
+# configs 
+# candidates config
+candidates_config = {
+    'reg': r'(\w+)candidate$',
+    'searcher': {
+        'baidu': {
+            'query':'https://www.baidu.com/s?ie=UTF-8&wd={query}',
+            'tips': 'Searcing in Baidu with "{query}"...'
+        },
+        'bing': {
+            'query':'https://cn.bing.com/search?q={query}',
+            'tips': 'Searcing in Bing with "{query}"...'
+        },
+        'google': {
+            'query':'https://www.baidu.com/s?ie=UTF-8&wd={query}',
+            'tips': 'Searcing in Google with "{query}"...'
+        },
+        'taobao': {
+            'query':'https://s.taobao.com/search?q={query}',
+            'tips': 'Searcing in Taobao with "{query}"...'
+        }
+    },
+    'handler': lambda res,tpl: re.sub('{query}', res, tpl)
+}
+
+# traslates
+translates_config = {
+    'reg': r'(\w+)translate',
+    'translater': {
+        'baidu': {
+            'url': 'http://fanyi.baidu.com/#en/zh/{keyword}',
+            'tips': 'Results: {keyword}'
+        }
+    },
+    'handler': lambda res,tpl: re.sub('{keyword}', res, tpl)
+}
+
+
 def error(msg = 'program occured a error'):
     print 'error:' + msg
     return msg
@@ -12,44 +50,113 @@ def responsePowerpack(res, argv):
         for e in res:
             data.append(e)
     else :
-        data.append(res)
+        data = res
     
-    for e in data:
-        items.append(outputFormat(argv[0], e))
+    items = outputFormat(argv, data)
+
     response = {'items':items}
     return json.dumps(response)
 
-def outputFormat(cmd, res):
-    data = {
-        'uid' :  native.md5(res),
-        'type' : 'default',
-        'title' : res,
-        'subtitle': '',
-        'autocomplete' : res,
-        'arg': res
-    }
-    # reg for jump to url
-    jump = [
-        {
-            'reg': r'(\w+)candidate$',
-            'urltplmap': {
-                'baidu': 'https://www.baidu.com/s?ie=UTF-8&wd={query}',
-                'bing': 'https://cn.bing.com/search?q={query}',
-                'google': 'https://www.baidu.com/s?ie=UTF-8&wd={query}',
-                'taobao': 'https://s.taobao.com/search?q={query}'
-                
-            },
-            'handler': lambda data,res,tpl: re.sub('{query}', res, tpl)
-        }
-    ]
+def outputFormat(argv, data):
+    # powerpack cmd
+    cmd = argv[0]
 
-    for e in jump:
-        reg = e['reg']
-        urltplmap = e['urltplmap']
-        
-        p = re.compile(reg, re.IGNORECASE)
-        matched = p.match(cmd)
-        if matched:
-            urltpl = urltplmap[matched.group(1)]
-            data['arg'] = e['handler'](data, res, urltpl)
-    return data
+    ret = candidatesFormat(argv, data)
+    if ret == False:
+        ret = translatesFormat(argv, data)
+
+    if ret == False:
+        ret = data
+        # try other
+
+
+    return ret
+
+def candidatesFormat(argv, data) :
+    # query replacement
+    tpl_render = candidates_config['handler']
+    # searcher config
+    searcher = False
+    # powerpack cmd
+    cmd = argv[0]
+    # object list for powerpack
+    ret = []
+    # search keyword
+    keyword = argv[1]
+
+    searchers = candidates_config['searcher']
+    # found searcher
+    reg = candidates_config['reg']
+    p = re.compile(reg, re.IGNORECASE)
+    matched = p.match(cmd)
+    if matched:
+        searcher = searchers[matched.group(1)]
+    # no matched searcher
+    if not searcher:
+        return False
+    
+    # formatting data (array) to popwerpack object list
+    # default search
+    ret.append({
+        'type' : 'default',
+        'title' : tpl_render(keyword, searcher['tips']),
+        'subtitle': '',
+        'autocomplete' : keyword,
+        'arg': tpl_render(keyword, searcher['query'])
+    })
+    for e in data:
+        data = {
+            'type' : 'default',
+            'title' : e,
+            'subtitle': '',
+            'autocomplete' : e,
+            'arg': tpl_render(e, searcher['query'])
+        }
+        ret.append(data)
+    return ret
+
+def translatesFormat(argv, data):
+     # keyword replacement
+    tpl_render = translates_config['handler']
+     # powerpack cmd
+    cmd = argv[0]
+    # object list for powerpack
+    ret = []
+    # search keyword
+    keyword = argv[1]
+    translater = False
+    # found details
+    reg = translates_config['reg']
+    p = re.compile(reg, re.IGNORECASE)
+    matched = p.match(cmd)
+    if matched:
+        translater = translates_config['translater'][matched.group(1)]
+    # no matched translater
+    if not translater:
+        return False
+    # formatting data (array) to popwerpack object list
+    # default search
+    translate_res = data['res']
+    top_item_tpl = translater['tips']
+    if len(data['correct']) > 0:
+        top_item_tpl = 'Looking for "' + data['correct'] +'"?:{keyword}'
+    
+    ret.append({
+        'type' : 'default',
+        'title' : tpl_render(translate_res, top_item_tpl),
+        'subtitle': 'Press enter to look details',
+        'autocomplete' : keyword if len(data['correct']) == 0 else data['correct'],
+        'arg': tpl_render(keyword, translater['url'])
+    })
+    for e in data['parts']:
+        means = '; '.join(e['means'])
+        title = e['part'] + ' ' + means
+        data = {
+            'type' : 'default',
+            'title' : title,
+            'subtitle': '',
+            'autocomplete' : '',
+            'arg': tpl_render(keyword, translater['url'])
+        }
+        ret.append(data)
+    return ret
